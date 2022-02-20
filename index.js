fc13d3f3bdb1961fda253ccb18547bf0381d05ad
clearan <clearan77@gmail.com>
2022-02-20 14:36:54 +0800

    fix

--- index.js
+++ index.js
@@ -3,11 +3,5 @@ console.log('+1')
 console.log('+2')
 console.log('+3')
 console.log(6)
-console.log(7)
-console.log(8)
-console.log(8)
-console.log(8)
-console.log(8)
-console.log(8)
-console.log(8)
+
 
